2cf2dfde7c005b8921f26a82b02b9ad0b9e0cd43
Jason <jay.jpy.wong@gmail.com>
2023-12-18 12:14:00 +0100

    Opzet sidebar

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".toggle");
+const sidebar = document.querySelector(".sidebar")
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
